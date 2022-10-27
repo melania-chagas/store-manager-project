b90fb58dd2cc9b637ee416167f886595f9d7eed3
Melania P Chagas <melaniapchagas@gmail.com>
2022-10-27 17:50:29 -0300

    Requisitos 1 e 2

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const { productRouter } = require('./router/products.router');
 
 app.use('/products', productRouter);
 
-// não remova esse endpoint, é para o avaliador funcionar.
+// não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
--- src/models/products.model.js
+++ src/models/products.model.js
@@ -1,11 +1,11 @@
 const connection = require('../connection');
 
 const modelGetAllProducts = async () => connection.execute(
-    'SELECT * FROM StoreManager.products',
+    'SELECT * FROM products',
   );
 
 const modelGetProductsById = async (id) =>
-  connection.execute(`SELECT * FROM StoreManager.products WHERE id = ${id}`);
+  connection.execute(`SELECT * FROM products WHERE id = ${id}`);
 
 module.exports = {
   modelGetAllProducts,
