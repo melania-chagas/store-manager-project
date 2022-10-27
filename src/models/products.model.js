487fad24d3f4216c3f773c1152ba7a5c971618e6
Melania P Chagas <melaniapchagas@gmail.com>
2022-10-27 18:24:41 -0300

    Nova tentativa - reqs 1 e 2

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -1,11 +1,11 @@
 const connection = require('../connection');
 
 const modelGetAllProducts = async () => connection.execute(
-    'SELECT * FROM products',
+    'SELECT * FROM StoreManager.products',
   );
 
 const modelGetProductsById = async (id) =>
-  connection.execute(`SELECT * FROM products WHERE id = ${id}`);
+  connection.execute(`SELECT * FROM StoreManager.products WHERE id = ${id}`);
 
 module.exports = {
   modelGetAllProducts,
