bafdb70b7f42ce9b71d75a689937ca229f7ef6e4
Melania P Chagas <melaniapchagas@gmail.com>
2022-10-27 19:13:38 -0300

    Nova tentativa: reqs 1 e 2

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const app = require('./app');
-const connection = require('./connection');
 
 // A biblioteca dotenv é um módulo Javascript que carrega variáveis de ambientes armazenadas em um arquivo .env para o process.env.
 require('dotenv').config();
@@ -8,10 +7,4 @@ require('dotenv').config();
 
 app.listen(process.env.PORT, async () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
-
-  // para testar a comunicação com o MySQL
-  const [result] = await connection.execute('SELECT 1');
-  if (result) {
-    console.log('MySQL connection ok');
-  }
 });
